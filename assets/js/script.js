517e361d201c26a497b3d0e206e4976667cc4d16
Murphy <sydneyporter74@yahoo.com>
2020-08-26 18:10:37 -0400

    updated taskformhandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,19 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createFormHandler = function(event) {
+var taskFormHandler = function(event) {
 
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+
     // package up data as an obect
     var taskDataObj = {
         name: taskNameInput,
@@ -37,7 +44,7 @@ var createTaskEl = function(taskDataObj) {
     tasksToDoEl.appendChild(listItemEl);  
 }
 
-formEl.addEventListener("submit", createFormHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 
 
